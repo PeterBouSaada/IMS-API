4142ac045ab37eccbfdd3bc0b1d9f3584064ca3f
peterbousaada <peterbousaada@gmail.com>
2022-09-06 23:15:17 -0400

    Fixed registration and sign in with devise

--- API_ruby/config/routes.rb
+++ API_ruby/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
+  namespace :api do
+    namespace :v1 do
+      devise_for :users
+    end
+  end
+  
   root to: "main#not_found"
 end
