369648ce88a6649ec886dca3ae6068de31b05838
peter bou saada <peterbousaada@gmail.com>
2023-02-18 18:48:08 -0500

    Removed everything to prepare repo

--- app/views/parts/_part.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! part, :id, :number, :category, :machine_id, :created_at, :updated_at
-json.url part_url(part, format: :json)
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  resources :parts
-  resources :machines
-  devise_for :users
-  root to: "home#index"
-end
